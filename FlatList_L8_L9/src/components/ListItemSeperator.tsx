4209d4a9a909c5057ef681cebff2c2ae6d574ef2
prishajha <prishajha1@gmail.com>
2026-09-14 09:30:39 -0500

    Initial commit

---
+++ FlatList_L8_L9/src/components/ListItemSeperator.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, View } from "react-native";
+
+const ListItemSeparator = () => {
+  return <View />;
+};
+
+export default ListItemSeparator;
+
+const styles = StyleSheet.create({
+  separator: {},
+});
