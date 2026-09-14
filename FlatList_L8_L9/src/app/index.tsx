abb55bb4b651dcfd8f7d64ee944e03069e25d467
prishajha <prishajha1@gmail.com>
2026-09-14 10:06:30 -0500

    created a basic FlatList, still need to add styling and then create custom components

--- FlatList_L8_L9/src/app/index.tsx
+++ FlatList_L8_L9/src/app/index.tsx
@@ -1,7 +1,49 @@
 import defaultStyles from "@/styles/defaultStyles";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Index() {
+  //inside the curly brace and before the return
+  //function is where you will put declare your
+  //fuctions and variables
+
+  //this generic datatype will represent the objects in FlatList
+
+  type dataType = {
+    id: string; //unique identifier so the list knows which item it is
+    title: string; //text or label for the object
+  };
+
+  //Create an array of the type dataType call is DATA
+  //using all caps because the array values won't change
+  //fill it with at least 3 items, need unique ID values
+  //each object will be in curly braces, seperated by commas, and
+  //you need to give a value to all the part of the dataType
+
+  const DATA: dataType[] = [
+    { id: "1", title: "First Item" },
+    { id: "2", title: "Second Item" },
+    { id: "3", title: "Third Item" },
+    { id: "4", title: "Fourth Item" },
+  ];
+
+  //add a state variable to keep track of what item has been selected
+  const [selectedID, setSelectedID] = useState<string>("");
+
+  //this simple function is called whenver the user clicks on the item
+  //in FlatList
+
+  const selectedList = (item: dataType) => {
+    setSelectedID(item.id);
+    console.log(item.title);
+  };
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -9,7 +51,16 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList
+            data={DATA}
+            keyExtractor={(item: dataType) => item.id}
+            extraData={selectedID}
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={() => selectedList(item)}>
+                <Text>{item.title}</Text>
+              </TouchableOpacity>
+            )}
+          />
         </View>
       </View>
     </View>
